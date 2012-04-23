85fb5fd6b6b723dc146fea2a255d048092c69a2a
KaitouKite <kite@mcblock.it>
2012-04-24 00:18:32 +0100

    Adding isStaff to lookup command

--- src/main/java/it/mcblock/mcblockit/bukkit/command/LookupCommand.java
+++ src/main/java/it/mcblock/mcblockit/bukkit/command/LookupCommand.java
@@ -16,11 +16,19 @@ public class LookupCommand implements CommandExecutor {
         }
 
         UserData userData = MCBlockItAPI.getUserData(args[0]);
-        sender.sendMessage(ChatColor.RED + "[MCBlockIt] " + ChatColor.WHITE + "User data for: " + ChatColor.AQUA + userData.getUsername());
-        sender.sendMessage(ChatColor.RED + "[MCBlockIt] " + ChatColor.DARK_RED + userData.getBans().length + ChatColor.WHITE + " ban(s), " + ChatColor.YELLOW + userData.getReputation() + ChatColor.WHITE + " reputation");
         BanData[] banData = userData.getBans();
-        for (BanData ban : banData) {
-            sender.sendMessage(ChatColor.RED + "[MCBlockIt] " + ChatColor.AQUA + ban.getServer() + ChatColor.WHITE + ": " + ban.getReason());
+
+        sender.sendMessage(ChatColor.RED + "[MCBlockIt] " + ChatColor.WHITE + "User " + ChatColor.AQUA + userData.getUsername() + ChatColor.WHITE + " has " + ChatColor.YELLOW + userData.getReputation() + ChatColor.WHITE + " reputation.");
+
+        if (userData.isStaff()) {
+            sender.sendMessage(ChatColor.RED + "[MCBlockIt] " + ChatColor.WHITE + userData.getUsername() + " is MCBlockIt Staff!");
+        }
+
+        if (banData.length > 0) {
+            sender.sendMessage(ChatColor.RED + "[MCBlockIt] " + ChatColor.DARK_RED + "Global Bans:");
+            for (BanData ban : banData) {
+                sender.sendMessage(ChatColor.RED + "[MCBlockIt] " + ChatColor.AQUA + ban.getServer() + ChatColor.WHITE + ": " + ban.getReason());
+            }
         }
 
         return true;
